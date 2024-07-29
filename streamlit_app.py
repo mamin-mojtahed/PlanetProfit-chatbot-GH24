625c3d951ee49fc102410bfad539a89091cdd2d1
M.Amin Mojtahed <mamin.mojtahed@gmail.com>
2024-07-29 04:51:37 +0000

    update st_pages import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from st_pages import Page, Section, show_pages, add_page_title, hide_pages
+from st_pages import Page, show_pages, add_page_title, hide_pages, Section
 from chatgpt import ask
 
 st.set_page_config(
